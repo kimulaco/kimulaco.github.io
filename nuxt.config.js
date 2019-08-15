cc31c6e5bdc9180a78dd872e297322e2388f3127
kimulaco <kmrkdev@gmail.com>
2019-08-15 21:48:00 +0900

    Revert "Update to pwa"
    This reverts commit 640831f358ee74165aba6f76ce0cbde75a2add01.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   mode: 'universal',
   head: {
-    title: 'kimulaco.me',
+    title: 'kimulaco',
     htmlAttrs: {
       lang: 'en',
       prefix: 'og: http://ogp.me/ns#'
@@ -15,31 +15,26 @@ export default {
         content: 'kimulaco profile site.'
       },
       { hid: 'og:type', property: 'og:type', content: 'website' },
-      { hid: 'og:title', property: 'og:title', content: 'kimulaco.me' },
+      { hid: 'og:title', property: 'og:title', content: 'kimulaco' },
       {
         hid: 'og:description',
         property: 'og:description',
         content: 'kimulaco profile site.'
       },
       { hid: 'og:url', property: 'og:url', content: 'http://kimulaco.me' },
-      { hid: 'og:image', property: 'og:image', content: '/img/ogp.png' },
-      { name: 'theme-color', content: '#333333' }
+      { hid: 'og:image', property: 'og:image', content: '/img/ogp.png' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/img/favicon.ico' }]
-  },
-  manifest: {
-    name: 'kimulaco.me',
-    lang: 'en'
   },
   loading: false,
   css: ['normalize.css', '@/assets/scss/base.scss'],
   plugins: [],
   devModules: ['@nuxtjs/eslint-module'],
   modules: [
+    // '@nuxtjs/pwa',
     '@nuxtjs/style-resources',
     'nuxt-webfontloader',
-    '@nuxtjs/google-analytics',
-    '@nuxtjs/pwa'
+    '@nuxtjs/google-analytics'
   ],
   styleResources: {
     scss: ['@/assets/scss/_variable.scss', '@/assets/scss/_mixin.scss']
